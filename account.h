91d157c430df92d8b35c2754b66e402e85f807a9
GUI123 <conanchanalfred@hotmail.com>
2015-06-23 22:30:10 -1000

    ICS 212 HW 5

---
+++ account.h
@@ -0,0 +1,32 @@
+/*****************************************************************
+ *
+ *  NAME:               Alfred Chan
+ *
+ *  HOMEWORK:       5
+ *
+ *  CLASS:              ICS 212
+ *
+ *  INSTRUCTOR:     Ravi Narayan
+ *
+ *  DATE:               June 23, 2015
+ *
+ *  FILE:                       hw2source.c
+ *
+ *  DESCRIPTION:     This file contains the source for the hw5
+ *
+ *
+ *****************************************************************/
+
+
+#ifndef ICS_212_HW_5_account_pch
+#define ICS_212_HW_5_account_pch
+
+
+struct account
+{
+    char              name[25];
+    int                 accountno;
+    float              balance;
+};
+
+#endif
---
+++ main.c
@@ -0,0 +1,33 @@
+/*****************************************************************
+ *
+ *  NAME:               Alfred Chan
+ *
+ *  HOMEWORK:       5
+ *
+ *  CLASS:              ICS 212
+ *
+ *  INSTRUCTOR:     Ravi Narayan
+ *
+ *  DATE:               June 23, 2015
+ *
+ *  FILE:                       hw2source.c
+ *
+ *  DESCRIPTION:     This file contains the header for the hw5
+ *
+ *
+ *****************************************************************/
+
+
+#include <stdio.h>
+#include "account.h"
+
+int main(int argc, char * argv[]) {
+    
+    
+    struct account         bankone[5];
+    int                    numcustomers;
+
+    
+    printf("Hello, World!\n");
+    return 0;
+}
