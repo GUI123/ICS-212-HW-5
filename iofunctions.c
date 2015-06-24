a3629017856af0e6cdd2bb4725bc70c2575e0a45
GUI123 <conanchanalfred@hotmail.com>
2015-06-24 00:21:02 -1000

    second commit

---
+++ iofunctions.c
@@ -0,0 +1,64 @@
+/*****************************************************************
+*
+*  NAME:               Alfred Chan
+*
+*  HOMEWORK:       5
+*
+*  CLASS:              ICS 212
+*
+*  INSTRUCTOR:     Ravi Narayan
+*
+*  DATE:               June 23, 2015
+*
+*  FILE:                       hw2source.c
+*
+*  DESCRIPTION:     This file contains the source for the hw2
+*
+*
+*****************************************************************/
+
+/*Protoypes*/
+void readfile(struct account accarray[ ], int * numcust, char filename[ ]);
+void writefile(struct account accarray[ ], int numcust, char filename[ ]);
+
+
+/*****************************************************************
+ *
+ *  Function name: readfile
+ *
+ *  DESCRIPTION:  reads a file
+ *
+ *  Parameters:   struct account accarray[]
+ *                int * numcust
+ *                char filename[]
+ *
+ *  Return values:   N/A
+ *
+ *
+ *****************************************************************/
+
+void readfile(struct account accarray[ ], int * numcust, char filename[ ])
+{
+    
+}
+
+/*****************************************************************
+ *
+ *  Function name: writefile
+ *
+ *  DESCRIPTION:  writes the file
+ *
+ *
+ *  Parameters:   struct account accarray[]
+ *                int * numcust
+ *                char filename[]
+ *
+ *  Return values:   N/A
+ *
+ *
+ *****************************************************************/
+void writefile(struct account accarray[ ], int numcust, char filename[ ]);
+{
+    
+}
+
